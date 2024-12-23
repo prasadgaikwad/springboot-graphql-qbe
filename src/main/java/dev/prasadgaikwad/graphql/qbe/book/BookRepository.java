4e4abc4225bc700aaecd5f7ad05aa0c938e80704
Prasad Gaikwad <prasad.gaikwad@gmail.com>
2024-12-23 12:02:12 -0600

    Initial commit

---
+++ src/main/java/dev/prasadgaikwad/graphql/qbe/book/BookRepository.java
@@ -0,0 +1,9 @@
+package dev.prasadgaikwad.graphql.qbe.book;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
+import org.springframework.graphql.data.GraphQlRepository;
+
+@GraphQlRepository
+public interface BookRepository extends JpaRepository<Book, Long>, QueryByExampleExecutor<Book> {
+}
